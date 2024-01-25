8adc9fa6af6076c747f93e8fe8d387eb7ec97c28
Ghulam Mujtaba <137277124+ghulammujtaba153@users.noreply.github.com>
2024-01-25 14:23:09 +0500

    commit

---
+++ person.py
@@ -0,0 +1,13 @@
+
+class Person:
+    def __int__(self, id, name, email, password, gender):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.password = password
+        self.gender = gender
+
+
+    def setId(self, id):
+        self.id=id
+
